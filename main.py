66dbfbc7b28bc9d2a9dd008b7476a6186e597c52
harshali <harshalik@335.com>
2025-12-11 10:20:00 +0530

    fourth commit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import arithmetic as arith
+import geomety as geo
 
 print("hello world! 🌸")
 
 arith.add(5,5)
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = geo.rectangle_area(length, width)
+perimeter = geo.rectangle_perimeter(length, width)
+
+print("Area of rectangle =", area)
+print("Perimeter of rectangle =", perimeter)
